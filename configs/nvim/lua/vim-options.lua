bcefcd57cc7ae883b009c7c8e16587a642a47035
Jen Reed <jennifer.reed@getinsured.com>
2024-08-06 13:27:52 -0400

    Changing out filetree plugin for now to nvim-tree

--- configs/nvim/lua/vim-options.lua
+++ configs/nvim/lua/vim-options.lua
@@ -1,7 +1,11 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
