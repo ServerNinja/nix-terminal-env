399edbb094751e57fd3f1b4b9054d49159486773
Jen Reed <jennifer.reed@getinsured.com>
2024-08-13 09:41:45 -0400

    Adding rust integrations

--- configs/nvim/lua/keymaps.lua
+++ configs/nvim/lua/keymaps.lua
@@ -9,3 +9,15 @@ keymap.set('n', '<leader>lg', '<cmd>LazyGit<cr>', {desc = "LazyGit"})
 
 -- Telescope
 keymap.set('n', '<C-p>', '<cmd>Telescope find_files<cr>', {})
+
+-- Rust
+local bufnr = vim.api.nvim_get_current_buf()
+vim.keymap.set(
+  "n", 
+  "<leader>a", 
+  function()
+    vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
+    -- or vim.lsp.buf.codeAction() if you don't want grouping.
+  end,
+  { silent = true, buffer = bufnr }
+)
