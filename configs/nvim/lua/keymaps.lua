f3ca21a17146c9e17ed6f8ca81efc8063ec8b1db
Jen Reed <jennifer.reed@getinsured.com>
2024-08-14 00:01:45 -0400

    Keymap management

--- configs/nvim/lua/keymaps.lua
+++ configs/nvim/lua/keymaps.lua
@@ -21,3 +21,29 @@ vim.keymap.set(
   end,
   { silent = true, buffer = bufnr }
 )
+
+-- WINDOW MANAGEMENT
+keymap.set("n", "<leader>sv", "<C-w>s", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>v", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>ss", "<C-x>", { desc = "Swap current window with next" }) -- swap window with next
+
+-- WINDOW Navigation
+keymap.set("n", "<leader>nk", "<C-w>k", { desc = "Window - Move up" }) -- window nav - move up
+keymap.set("n", "<leader>nj", "<C-w>j", { desc = "Window - Move down" }) -- window nav - move down
+keymap.set("n", "<leader>nh", "<C-w>h", { desc = "Window - Move left" }) -- window nav - move left
+keymap.set("n", "<leader>nl", "<C-w>l", { desc = "Window - Move right" }) -- window nav - move right
+
+-- Copilot
+keymap.set("n", "<leader>Ct", function() require("CopilotChat").toggle() end, { desc = "Copilot - Toggle" })
+keymap.set("n", "<leader>Cq", function() require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer }) end, { desc = "Copilot - Quick Chat" })
+
+keymap.set('n', "<leader>nm", function() require('nvim-window').pick() end, {desc = "nvim-window: Jump to window"})
+
+-- Tab management / BarBar.nvim (Tab based buffer management and navigation)
+keymap.set('n', '<leader>th', '<Cmd>BufferPrevious<CR>', {desc = "Go to previous buffer"})
+keymap.set('n', '<leader>tl', '<Cmd>BufferNext<CR>', {desc = "Go to next buffer"})
+keymap.set('n', '<leader>tc', '<Cmd>BufferClose<CR>', {desc = "Close buffer"})
+keymap.set('n', '<leader>tp', '<Cmd>BufferPick<CR>', {desc = "Pick buffer"})
+keymap.set('n', '<leader>tn', '<cmd>tabnew<CR>', {desc = "New tab"})
--- configs/nvim/lua/plugins/which-key.lua
+++ configs/nvim/lua/plugins/which-key.lua
@@ -2,10 +2,38 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    -- This isn't working for some reason --- ? WTF
+    win = {
+--      no_overlap = true,
+      border = "single",
+      padding = { 1, 2 },
+      title = true,
+      title_pos = "center",
+      zindex = 1000,
+    },
   },
+  config = function(_, opts)
+    local wk = require("which-key")
+    -- As an example, we will create the following mappings:
+    --  * <leader>ff find files
+    --  * <leader>fr show recent files
+    --  * <leader>fb Foobar
+    -- we'll document:
+    --  * <leader>fn new file
+    --  * <leader>fe edit file
+    -- -- and hide <leader>1
+   
+    wk.add({
+    -- group renames
+      { "<leader>C", group = "Copilot Chat" },
+      { "<leader>l", group = "Lazy"},
+      { "<leader>n", group = "Window Navigation", icon = { icon = "", color = "azure" } },
+      { "<leader>s", group = "Split", icon = { icon = "", color = "purple" } },
+      { "<leader>t", group = "Tab Management", icon = { icon = "", color = "green" } },
+      { "<leader>w", group = "Window Management", icon = { icon = "", color = "azure" } },
+    }, { prefix = "<leader>" })
+    wk.setup(opts)
+  end,
   keys = {
     {
       "<leader>?",
