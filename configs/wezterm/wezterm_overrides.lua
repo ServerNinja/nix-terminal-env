0bcf2fcab00e1691af68f0574c61f033572f5a60
Jen Reed <serverninja@gmail.com>
2024-09-26 18:48:38 -0400

    Adding some more wezterm overrides

--- configs/wezterm/wezterm_overrides.lua
+++ configs/wezterm/wezterm_overrides.lua
@@ -1,4 +1,24 @@
 return {
-  -- Font Size
-  font_size = 10.0,
+  ---- Tab bar
+  --enable_tab_bar = true,
+  --use_fancy_tab_bar = false,
+
+  ---- Font Size
+  --font = "Hack Nerd Font Mono",
+  --font_size = 10.0,
+
+  ---- Scrollback lines (buffer)
+  --scrollback_lines = 50000,
+  --enable_scroll_bar = true,
+
+  ---- Initial window size
+  --window_initial_rows = 27,
+  --window_initial_cols = 100,
+
+  ---- Color Schemes
+  --dark_mode_color_scheme = "Lavandula (Gogh)",
+  --dark_mode_background_opacity = 0.9,
+
+  --light_mode_color_scheme = "Summerfruit Light (base16)",
+  --light_mode_background_opacity = 1.0,
 }
