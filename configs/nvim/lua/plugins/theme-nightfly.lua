e9af755469bf185fab74b675a6c72a6663dfb89c
Jen Reed <serverninja@gmail.com>
2024-09-26 19:04:09 -0400

    Adding a few more themes for now

---
+++ configs/nvim/lua/plugins/theme-nightfly.lua
@@ -0,0 +1,12 @@
+-- nightfly nvim theme
+--
+-- The NightFly theme for Neovim
+-- Link:  plugin
+--
+-- Notes: Setting themes in auto-dark-mode configuration
+return {
+  "bluz71/vim-nightfly-colors",
+  name = "nightfly",
+  lazy = false,
+  priority = 1000
+}
---
+++ configs/nvim/lua/plugins/theme-rosepine.lua
@@ -0,0 +1,10 @@
+-- rosepine nvim theme
+--
+-- The RosePine theme for Neovim
+-- Link:  plugin
+--
+-- Notes: Setting themes in auto-dark-mode configuration
+return {
+  "rose-pine/neovim",
+  name = "rose-pine"
+}
